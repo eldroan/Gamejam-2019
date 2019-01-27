37c98e1c45b342a8a302eed803235faee0015cb6
MATO-PC\Mato <matiasmato@gmail.com>
2019-01-27 02:38:14 -0300

    Se añade la chancla voladora

---
+++ GameJam2019/Assets/Scripts/BluntObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluntObject : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3f;
+
+    private bool travelling = false;
+
+    private void Update()
+    {
+        if (travelling)
+        {
+            this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+    public void Shoot()
+    {
+        this.travelling = true;
+    }
+}
