983cd20e8ead34e97a873d6ac4b43a27f81ad533
eldroan <leandroamarillo95@gmail.com>
2019-01-27 09:04:45 -0300

    Sound Fx a los prefabs

---
+++ GameJam2019/Assets/Scripts/PlayerSoundFX.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundFX : MonoBehaviour
+{
+    [SerializeField] public AudioClip jump;
+    [SerializeField] public AudioClip attack1;
+    [SerializeField] public AudioClip attack2;
+    [SerializeField] public AudioClip block;
+    [SerializeField] public AudioClip damageRecieved;
+}
