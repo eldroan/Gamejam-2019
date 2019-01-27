17b01ba1c59262d923f757c8971bcc8d8bb05b1c
MATO-PC\Mato <matiasmato@gmail.com>
2019-01-27 08:05:43 -0300

    Se arreglan las animaciones

---
+++ GameJam2019/Assets/DarkestScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkestScript : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController playerController;
+
+    public void InAttackMoment()
+    {
+        playerController.InAttackMoment();
+    }
+
+    public void EndBlock()
+    {
+        playerController.EndBlock();
+    }
+
+    public void EndAttack()
+    {
+        playerController.EndAttack();
+    }
+}
